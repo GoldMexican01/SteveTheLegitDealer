dcb966bde05acb538d490825ada9e7d36fbd145e
Ollie Pearson <Ollie170@outlook.com>
2018-09-17 10:15:16 +0100

    Its working?

--- Stevethedealer/Module1.vb
+++ Stevethedealer/Module1.vb
@@ -2,22 +2,47 @@
 
     Sub Main()
         Dim cardsuit As Integer
+        Dim cardnum As Integer
         Dim suit As String
-        Randomize()
-        cardsuit = Int(4 * Rnd() + 1) 'this generates the suit
-        If cardsuit = "1" Then
-            suit = "Hearts"
-        ElseIf cardsuit = "2" Then
-            suit = "Tiamonds"
-        ElseIf cardsuit = "3" Then
-            suit = "Clubs"
-        ElseIf cardsuit = "4" Then
-            suit = "Spades"
-        End If
+        Dim card As String
+        Console.BackgroundColor = ConsoleColor.White
+        For deal = 1 To 6
+            Randomize()
+            cardsuit = Int(4 * Rnd() + 1) 'this generates the suit
+            Randomize()
+            cardnum = Int(13 * Rnd() + 1)
 
+            If cardsuit = "1" Then 'This gives the suit number a friendly name and a color
+                suit = "Hearts"
+                Console.ForegroundColor = ConsoleColor.DarkRed
+            ElseIf cardsuit = "2" Then
+                suit = "Diamonds"
+                Console.ForegroundColor = ConsoleColor.DarkRed
+            ElseIf cardsuit = "3" Then
+                suit = "Clubs"
+                Console.ForegroundColor = ConsoleColor.Black
+            ElseIf cardsuit = "4" Then
+                suit = "Spades"
+                Console.ForegroundColor = ConsoleColor.Black
+            End If
 
-        Console.WriteLine(cardsuit)
+            If cardnum = "1" Then 'This gives the card number a friendly name
+                card = "Ace"
+            ElseIf cardnum = "11" Then
+                card = "Jack"
+            ElseIf cardnum = "12" Then
+                card = "Queen"
+            ElseIf cardnum = "13" Then
+                card = "King"
+            Else
+                card = cardnum
+            End If
+
+            Console.WriteLine(card & " of " & suit)
+        Next
         Console.ReadLine()
+
+
 
 
 
