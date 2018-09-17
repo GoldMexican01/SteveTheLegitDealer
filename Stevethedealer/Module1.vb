8e0201c4594f9bb31176a2ce6b8820d23a11b4b5
Ollie Pearson <Ollie170@outlook.com>
2018-09-17 09:31:37 +0100

    Base version (Not Working)

---
+++ Stevethedealer/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim cardsuit As Integer
+        Dim suit As String
+        Randomize()
+        cardsuit = Int(4 * Rnd() + 1) 'this generates the suit
+        If cardsuit = "1" Then
+            suit = "Hearts"
+        ElseIf cardsuit = "2" Then
+            suit = "Tiamonds"
+        ElseIf cardsuit = "3" Then
+            suit = "Clubs"
+        ElseIf cardsuit = "4" Then
+            suit = "Spades"
+        End If
+
+
+        Console.WriteLine(cardsuit)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
